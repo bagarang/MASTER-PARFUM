7c2a075965c8a4cbb48b1afd3aa3fb317b7be8f5
bagarang <110575081+bagarang@users.noreply.github.com>
2026-07-03 17:01:18 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
    Contoh: "https://script.google.com/macros/s/AKfycb.../exec"
    ===================================================================== */
 const APP_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxlAsTnpsLC4jEHOLAEAh7oQLgCUDLwALeQA1V2Tp_s6zlYDKXKzPwEseANW8NEad8/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw6E0hnUwAp9KiCAUyKPx9e0tscnG-1SYBsBtlE7rQtqNH1VNiK9ul8V5wsuQsU7D4/exec",
   APP_NAME: "Master Parfum",
   SESSION_KEY: "mp_session_v1"
 };
